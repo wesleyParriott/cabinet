020a8e7f5e2ea182f72e62f17b53706279eefaac
vvesley <wesleyprrtt@gmail.com>
2024-08-11 12:25:57 -0500

    log: log level can be set by a flag

--- flag.go
+++ flag.go
@@ -10,6 +10,7 @@ var (
 	HelpFlag      bool
 	SetupFlag     bool
 	VersionFlag   bool
+	LogLevel      string
 )
 
 func PrintUsage() {
@@ -20,10 +21,18 @@ func PrintUsage() {
 func initFlags() {
 	flag.BoolVar(&BreakdownFlag, "b", false, "breaks down daemon")
 	flag.BoolVar(&BreakdownFlag, "breakdown", false, "breaks down daemon")
+
 	flag.BoolVar(&HelpFlag, "h", false, "prints this helpful message")
 	flag.BoolVar(&HelpFlag, "help", false, "prints this helpful message")
+
 	flag.BoolVar(&SetupFlag, "s", false, "sets up daemon")
 	flag.BoolVar(&SetupFlag, "setup", false, "sets up daemon")
+
 	flag.BoolVar(&VersionFlag, "v", false, "prints version")
 	flag.BoolVar(&VersionFlag, "version", false, "prints version")
+
+	flag.StringVar(&LogLevel, "loglevel", "debug", "set log level")
+	flag.StringVar(&LogLevel, "l", "debug", "set log level")
+
+	flag.Parse()
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"net/http"
 	"os"
 
@@ -13,7 +12,26 @@ var Logger wlog.Wlog
 
 func init() {
 	initFlags()
-	Logger = wlog.CreateWlogWithParams(os.Stdout, wlog.DEBUG)
+
+	var logLevel int
+
+	if LogLevel == "debug" {
+		// the default of the flag
+		// so if they don't set anything it should go here
+		logLevel = wlog.DEBUG
+	} else if LogLevel == "info" {
+		logLevel = wlog.INFO
+	} else if LogLevel == "error" {
+		logLevel = wlog.ERROR
+	} else if LogLevel == "fatal" {
+		logLevel = wlog.FATAL
+	} else {
+		println("Sorry didn't recognize " + LogLevel + " as a log level. Please use these:")
+		println("\tdebug, info, error, or fatal")
+		os.Exit(0)
+	}
+
+	Logger = wlog.CreateWlogWithParams(os.Stdout, logLevel)
 	err := setPasscode()
 	if err != nil {
 		Logger.Fatal("during init couldn't set passcode because: %s", err)
@@ -23,7 +41,6 @@ func init() {
 func main() {
 	version := "v0.2.1"
 
-	flag.Parse()
 	if HelpFlag {
 		PrintUsage()
 	}
