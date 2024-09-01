48e51a28524e0d18dfc8ee3c19c13c79697147f9
vvesley <wesleyprrtt@gmail.com>
2024-09-01 18:50:02 -0500

    add some http utils

---
+++ httpUtils.go
@@ -0,0 +1,25 @@
+package main
+
+import "net/http"
+
+// 200s
+func Okay(response http.ResponseWriter, message []byte) {
+	response.Write(message)
+}
+
+// 300s
+// ...
+
+// 400s
+func BadRequest(response http.ResponseWriter) {
+	http.Error(response, http.StatusText(400), 400)
+}
+
+func EntityTooLarge(response http.ResponseWriter) {
+	http.Error(response, http.StatusText(413), 413)
+}
+
+// 500s
+func InternalError(response http.ResponseWriter) {
+	http.Error(response, http.StatusText(500), 500)
+}
